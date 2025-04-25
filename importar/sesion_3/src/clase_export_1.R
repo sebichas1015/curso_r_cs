f840b12f05268bf0ba5e12e4bad145e1205d22d7
sebichas1015 <sebastian.phyp@gmail.com>
2025-04-25 08:36:38 -0500

    add clase_export_1

--- importar/sesion_3/src/clase_export_1.R
+++ importar/sesion_3/src/clase_export_1.R
@@ -18,11 +18,13 @@ input_ins_viol <- "input_data/violencia/Replication Dataset-Panel Data.dta"
 
 input_censo <- "input_data/agropecuario/censo_agro_import.parquet"
 
-output_hogares <- "introduccion/output/s15h_hogares.parquet"
+output_pnis <- "importar/sesion_3/output/pnis.parquet"
 
-output_coca <- "introduccion/output/coca_anio_mpio.parquet"
+output_hogares <- "importar/sesion_3/output/s15h_hogares.csv"
 
-output_censo <- "introduccion/output/censo_agro_vars.parquet"
+output_ins_viol <- "importar/sesion_3/output/ins_viol.xlsx"
+
+output_censo <- "importar/sesion_3/output/censo_agro_vars.ods"
 
 log_info("LOAD DATA")
 table_pnis <- read.xlsx(input_pnis)
@@ -35,28 +37,36 @@ table_censo <- read_parquet(input_censo,
                             col_select = c("recordid", "p_s12p150a",
                                            "s05_tenencia"))
 
-log_info("CREATE RECORD NUMBER")
-table_hogares <- table_hogares %>%
-  mutate(id_record = row_number()) %>%
-  verify(nrow(.) == n_table_hogares)
+log_info("FIRST 5 RECORDS")
+table_pnis <- table_pnis %>% 
+  head()
 
-table_coca <- table_coca %>%
-  mutate(id_record = row_number()) %>%
-  verify(nrow(.) == n_table_coca)
+table_hogares <- table_hogares %>% 
+  head()
 
-table_censo <- table_censo %>%
-  mutate(id_record = row_number()) %>%
-  verify(nrow(.) == n_table_censo)
+table_ins_viol <- table_ins_viol %>% 
+  head()
+
+table_censo <- head(table_censo)
 
 log_info("EXPORT")
-write_parquet(table_hogares, output_hogares)
+write_parquet(table_pnis, output_pnis)
 
-write_parquet(table_coca, output_coca)
+write_csv(table_hogares, output_hogares)
 
-write_parquet(table_censo, output_censo)
+write_xlsx(table_ins_viol, output_ins_viol)
+
+
+wb <- createWorkbook()
+
+addWorksheet(wb, "hoja_censo")
+
+writeData(wb, "hoja_censo", table_censo)
+
+saveWorkbook(wb, output_censo, overwrite = TRUE)
 
 rm(table_hogares, table_coca, table_censo)
 
 gc()
 
-log_info("DONE clase_import_export.R")
+log_info("DONE clase_export.R")
