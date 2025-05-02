3d07af72a9ce82805711fa0163b4a534390ff70a
sebichas1015 <sebastian.phyp@gmail.com>
2025-05-02 14:05:48 -0500

    add new paths

--- importar/sesion_3/src/clase_import_2.R
+++ importar/sesion_3/src/clase_import_2.R
@@ -9,9 +9,9 @@ pacman::p_load(dplyr, logger, assertr, arrow, writexl, readr, openxlsx, haven)
 log_info("DEFINE DIRECTORY")
 setwd("/Users/sebas/OneDrive/Documents/curso_r_cs/")
 
-input_pnis <- "input_data/economias_ilicitas/Datos-anónimos-encuesta-PNIS.xlsx"
+input_coca <- "input_data/economias_ilicitas/RPT_CultivosIlicitos_2024-12-22--194158.xlsx"
 
-input_hogares <- "input_data/agropecuario/S15H(Hogares).csv"
+input_gini_t <- "input_data/agropecuario/GINI_COL_IGAC_V3_07052024.xlsx"
 
 input_ins_viol <- "input_data/violencia/Replication Dataset-Panel Data.dta"
 
@@ -20,15 +20,11 @@ input_cnmh_df <- "input_data/violencia/CasosDF_202409.parquet"
 input_censo <- "input_data/agropecuario/censo_agro_import.parquet"
 
 log_info("LOAD DATA")
-table_pnis <- read.xlsx(input_pnis)
-
-table_hogares <- read_delim(input_hogares)
-
-table_ins_viol <- read_dta(input_ins_viol)
-
-table_cnmhdf <- read_parquet(input_cnmh_df)
-
 table_censo <- read_parquet(input_censo, col_select = c("recordid", "p_s12p150a", "s05_tenencia"))
+
+table_coca <- read.xlsx(input_coca, startRow = 9)
+
+table_gini <- read.xlsx(input_gini_t, sheet = "Gini Municipal", startRow = 4)
 
 log_info("DELETE OBJECTS")
 rm(table_pnis, table_hogares, table_ins_viol, table_cnmhdf, table_censo)
